c433725033636656bdf19e6bd3235338ca99c3ff
Maxim Bodrov <activer12@mail.ru>
2026-04-13 18:37:19 +0300

    feat: new core package use

--- core/core/spec_time.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timezone, timedelta
-
-
-def get_current_time():
-    return datetime.now(timezone.utc)
-
-
-def time_with_shift(duration_days: int):
-    return datetime.now() + timedelta(days=duration_days)
